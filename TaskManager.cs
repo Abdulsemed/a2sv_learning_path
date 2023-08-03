2fcdd2bd34878c95b4cbe00bb360d84a7c60aa42
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-08-03 22:34:04 +0300

    final update of task manager

--- TaskManager.cs
+++ TaskManager.cs
@@ -1,4 +1,9 @@
 using System;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
 class MainClass
 {
     public enum Category
@@ -8,7 +13,7 @@ class MainClass
         Errand,
         unknown
     }
-    public class Task
+    public class Tasks
     {
         public string Name { get; set; } = "";
         public string Description { get; set; } = "";
@@ -17,20 +22,90 @@ class MainClass
 
 
     }
+    // class for manipulating files
+    public class FileManager
+    {
+        public async Task WriteToFile(FileStream fs, List<Tasks> tasks)
+        {
+            //file open and write to Tasks
+            foreach (Tasks task in tasks)
+            {
+                byte[] encodedText = Encoding.Unicode.GetBytes(task.Name + " " + task.Description + " " + task.Category + " " + task.IsCompleted+"\n");
+                await fs.WriteAsync(encodedText);
+            }
+
+            
+        }
+        // method read tasks from txt and append the to taks list
+        public async Task ReadFile(FileStream fs, List<Tasks> tasks)
+        {
+            string text;
+            var streamReader = new StreamReader(fs);
+            string name;
+            string description;
+            Category category;
+            bool isCompleted = false;
+            while((text = await streamReader.ReadLineAsync()) != null)
+            {
+                string[] lists = text.Split(" ");
+                name = lists[0];
+                description = lists[1];
+                category =(Category) Enum.Parse(typeof(Category), lists[2]);
+                if (lists[3] == "true")
+                {
+                    isCompleted = true;
+                }
+                Tasks task = new Tasks() { Name = name, Category = category, Description = description, IsCompleted = isCompleted };
+                tasks.Add(task);
+            }
+
+        }
+    }
+    // class to manage the tasks
 
     public class TaskManager
     {
-        public List<Task> Tasks { get; set; }
+        public List<Tasks> Tasks = new List<Tasks>();
+        private string nameOfFile = "tasks.txt";
+        FileManager fileManager = new FileManager();
         public TaskManager()
         {
-            //file open and write to Tasks
+            if (!File.Exists(nameOfFile))
+            {
+                File.Create(nameOfFile).Close();
+            }
         }
-        public void AddTask(Task task)
+        // an initial function to read tasks from file
+        public async void Init()
         {
-            // file read and write here
-            Tasks.Add(task);
+            try
+            {
+                FileStream fs = File.Open(nameOfFile, FileMode.Open, FileAccess.ReadWrite);
+                await fileManager.ReadFile(fs, Tasks);
+                fs.Close();
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
-        private void Display(List<Task> param)
+
+       
+        public async void AddTask(Tasks task)
+        {
+            try
+            {
+                FileStream fs = File.Open(nameOfFile, FileMode.Open, FileAccess.ReadWrite);
+                Tasks.Add(task);
+                await fileManager.WriteToFile(fs, Tasks);
+                fs.Close();
+            }
+            catch (Exception ex) { 
+                Console.WriteLine(ex.Message);
+            }
+            // file read and write here
+            
+        }
+        private void Display(List<Tasks> param)
         {
             Console.WriteLine("Name \t description \t category \t isCompleted");
             foreach (var task in param)
@@ -42,13 +117,13 @@ class MainClass
         {
             Display(Tasks);
         }
-
+        // function to filter tasks based on the category
         public void Filtering(Category category)
         {
-            var filtered = Tasks.Where(task => task.Category == category);
+            var filtered = Tasks.Where(task => task.Category == category).ToList();
             if (filtered != null)
             {
-                Display((List<Task>)filtered);
+                Display(filtered);
             }
             else
             {
@@ -56,19 +131,22 @@ class MainClass
             }
 
         }
-        public Task Search(string query)
+        public Tasks Search(string query)
         {
-            Task? task = Tasks.Find(task => task.Name == query);
+            Tasks? task = Tasks.Find(task => task.Name == query);
             return task;
         }
 
-        public void Updating(Task oldTask, Task updatedTask)
+        public async void Updating(Tasks oldTask, Tasks updatedTask)
         {
-            
-            // file read, delete and update here
-
+            Tasks[Tasks.IndexOf(oldTask)] = updatedTask;
+            FileStream fs = File.Open(nameOfFile, FileMode.Open, FileAccess.ReadWrite);
+            // file read and write here
+            await fileManager.WriteToFile(fs, Tasks);
+            fs.Close();
         }
     }
+    // function to accept and validate string inputs
     public static string inputAccepeter(string param)
     {
         string input = "";
@@ -87,6 +165,7 @@ class MainClass
         } while (string.IsNullOrEmpty(input));
         return input;
     }
+    // function to accept and validate integer inputs returns a default 3
     public static int forInt(string str)
     {
         int value;
@@ -101,7 +180,8 @@ class MainClass
         return value;
 
     }
-    public static Task addTaskParams()
+    //method to create tasks
+    public static Tasks addTaskParams()
     {
         string name;
         string description;
@@ -115,23 +195,32 @@ class MainClass
         {
             complete = true;
         }
-        Task task = new Task() { Name = name, Category = category, Description = description, IsCompleted = complete };
+        Tasks task = new Tasks() { Name = name, Category = category, Description = description, IsCompleted = complete };
         return task;
     }
     public static void Main(string[] args)
     {
         TaskManager manager = new TaskManager();
+        manager.Init();
         bool flag = true;
+        int choice = 0;
         do
         {
             Console.WriteLine("enter your choice");
             Console.WriteLine(" 1. add a Task  \n 2. view Tasks \n 3. filter tasks \n 4. update a task \n other. exit");
-            int choice = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+               choice = Convert.ToInt32(Console.ReadLine());
+            }catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message +" message end");
+            }
+            
             if (choice < 5 && choice > 0)
             {
                 if (choice == 1)
                 {
-                    Task task = addTaskParams();
+                    Tasks task = addTaskParams();
                     manager.AddTask(task);
 
                 }
@@ -147,7 +236,7 @@ class MainClass
                 }
                 else if (choice == 4)
                 {
-                    Task task = manager.Search(inputAccepeter("name of task to search"));
+                    Tasks task = manager.Search(inputAccepeter("name of task to search"));
                     if (task != null)
                     {
                         manager.Updating(task, addTaskParams());

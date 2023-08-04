100191407ab12e99b775f215d0b60a89b84c3b86
Abdulsemed <88775958+Abdulsemed@users.noreply.github.com>
2023-08-04 21:08:43 +0300

    Update StudentManagementSystem.cs

--- StudentManagementSystem.cs
+++ StudentManagementSystem.cs
@@ -16,22 +16,25 @@ class MainClass
             Age = age;
             Grade = grade;
         }
-
+        public override string ToString()
+        {
+            return $"{Rollnumber} \t {Name} \t {Age} \t {Grade}";
+        }
     }
     class StudentList<T>
     {
-        List<Student> students = new List<Student>();
+        public List<T> students = new List<T>();
 
-        public void AddStudent(Student student)
+        public void AddStudent(T student)
         {
             students.Add(student);
         }
-        private void DisplayStudent(List<Student> item)
+        private void DisplayStudent(List<T> item)
         {
-            Console.WriteLine("Name \t description \t category \t isCompleted");
+            Console.WriteLine("RollNumber \t Name \t Age \t Grade");
             foreach (var student in item)
             {
-                Console.WriteLine($"{student.Rollnumber} \t {student.Name} \t {student.Age} \t {student.Grade}");
+                Console.WriteLine(student.ToString());
             }
         }
         public void ViewStudent()
@@ -43,9 +46,32 @@ class MainClass
             return students.Count-1;
         }
 
-        public static void SearchStudent(string query)
+        public static void SearchStudentById(List<Student> studentList,int query)
         {
-            //handle cases
+
+           IEnumerable<Student> result = from student in studentList where student.Rollnumber == query select student;
+           if(result.Count() == 1)
+            {
+                Console.WriteLine(result.ElementAt(0).ToString());
+            }
+            else
+            {
+                Console.WriteLine($"{query} was not found");
+            }
+
+        }
+
+        public static void SearchStudentByName(List<Student> studentList,string name)
+        {
+            IEnumerable<Student> result = from student in studentList where student.Name == name select student;
+            if (result.Count() == 1)
+            {
+                Console.WriteLine(result.ElementAt(0).ToString());
+            }
+            else
+            {
+                Console.WriteLine($"{name} was not found");
+            }
         }
     }
     public static string inputAccepeter(string param)
@@ -74,9 +100,9 @@ class MainClass
         {
             return 1.0;
         }
-        else if (value < 0 && value < 3)
+        else if (value < 0 && value > 4.0)
         {
-            return 3;
+            return 1.0;
         }
         return value;
 
@@ -84,13 +110,13 @@ class MainClass
 
     public static void Main(string[] args)
     {
-        StudentList<List<Student>> studentList = new StudentList<List<Student>>();
+        StudentList<Student> students = new StudentList<Student>();
         bool flag = true;
         int choice = 0;
         do
         {
             Console.WriteLine("enter your choice");
-            Console.WriteLine(" 1. add a student  \n 2. view added students \n 3. search students by id or name \n other. exit");
+            Console.WriteLine(" 1. add a student  \n 2. view added students \n 3. search students by name \n 4. search students by id \n other. exit");
             try
             {
                 choice = Convert.ToInt32(Console.ReadLine());
@@ -107,17 +133,20 @@ class MainClass
                     string name = inputAccepeter("name of student");
                     double grade = ForDouble(inputAccepeter("enter student grade"));
                     int age = (int)ForDouble(inputAccepeter("enter student age"));
-                    Student student = new Student(studentList.Count()+1, name, age, grade);
-                    studentList.AddStudent(student);
+                    Student student = new Student(students.Count()+1, name, age, grade);
+                    students.AddStudent(student);
                 }
                 else if (choice == 2)
                 {
-                    studentList.ViewStudent();
+                    students.ViewStudent();
                 }
                 else if (choice == 3)
                 {
-                    StudentList<Student>.SearchStudent(inputAccepeter("name or id to search for"));
+                    StudentList<Student>.SearchStudentByName(students.students,inputAccepeter("name of student to search"));
 
+                }else if(choice == 4)
+                {
+                    StudentList<Student>.SearchStudentById(students.students,(int)ForDouble(inputAccepeter("id of student to search")));
                 }
 
             }
